318501f860c9cf49ae5eee076fdce8162bd4e6d8
MondoBazoo <isaiahrivera850@gmail.com>
2022-05-30 15:22:41 -0700

    added new codewars challenge

---
+++ 8kyu/function3-multiplying-two-numbers.js
@@ -0,0 +1,22 @@
+//Implement a function which multiplies two numbers.
+
+//My solution
+function multiply(num1,num2){
+    return num1 * num2
+    }
+
+//Top 3 solutions 
+//1
+let multiply = (a, b) => a * b;
+
+//2
+function multiply(a, b){
+    if(typeof a == 'number' && typeof b == 'number')
+      return a * b;  
+  }
+
+//3
+// returns product of two arguments a and b
+function multiply(a, b){
+    return a*b;
+  }
