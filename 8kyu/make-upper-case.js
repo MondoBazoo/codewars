0d65aa6227ed6e7b1544864e1e0c928721923598
MondoBazoo <isaiahrivera850@gmail.com>
2022-06-05 18:53:38 -0700

    added new codewars challenge

---
+++ 8kyu/make-upper-case.js
@@ -0,0 +1,16 @@
+//Write a function which converts the input string to uppercase.
+
+//My solution
+function makeUpperCase(str) {
+    return str.toUpperCase();
+   }
+
+//Top 3 solutions
+//1
+const makeUpperCase = str => str.toUpperCase();
+//2
+function makeUpperCase(str) {return str.toUpperCase();}
+//3
+function makeUpperCase(str) {
+    return str.toLocaleUpperCase();
+  }
