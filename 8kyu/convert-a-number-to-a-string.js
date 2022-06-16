7e3e361910aeef1941d55acf3c80f881a59878fa
MondoBazoo <isaiahrivera850@gmail.com>
2022-06-15 18:07:49 -0700

    added new codewars challenge

---
+++ 8kyu/convert-a-number-to-a-string.js
@@ -0,0 +1,26 @@
+//We need a function that can transform a number into a string.
+//What ways of achieving this do you know?
+//Examples:
+//123 --> "123"
+//999 --> "999"
+
+//My solution
+function numberToString(num) {
+    return num.toString();
+  }
+
+//Top 3 solutions
+//1
+function numberToString(num) {
+    return num.toString();
+}
+//2
+function numberToString(num) {
+    // Return a string of the number here!
+    return String(num);
+  }
+//3
+function numberToString(num) {
+    return ''+num;
+  }
+  
