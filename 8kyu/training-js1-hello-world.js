5537a79444e3044dc721ac21a69d2f61bd0e1960
MondoBazoo <isaiahrivera850@gmail.com>
2022-06-21 15:43:44 -0700

    added new codewars challenge

---
+++ 8kyu/training-js1-hello-world.js
@@ -0,0 +1,33 @@
+//Task
+//Please refer to two example above and write your first JS function.
+//mission 1:
+//use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
+//mission 2:
+//use keyword var (or let or const) to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+//mission 3:
+//type the console.log() in the next line(don't forget to put the str in the brackets).
+//When you have finished the work, click "Run Tests" to see if your code is working properly.
+//In the end, click "Submit" to submit your code pass this kata.
+
+//My solution
+function helloWorld(){
+    let str = "Hello World!"
+    console.log(str)
+  }
+
+//Top 3 solutions
+//1
+helloWorld = function() {
+    var str = "Hello World!";
+    console.log( str );
+  }
+//2
+function helloWorld(){
+    var str="Hello World!";
+    console.log(str);
+  }
+//3
+const helloWorld = () => {
+    var str = `Hello World!`;
+    console.log(str);
+  };
