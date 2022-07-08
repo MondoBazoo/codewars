14638b40d6d137acbe928fdfa136175c3fbc723b
MondoBazoo <isaiahrivera850@gmail.com>
2022-07-08 11:51:47 -0700

    added new codewars challenge

---
+++ 7kyu/strings-ends-with.js
@@ -0,0 +1,20 @@
+//Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+//Examples:
+//solution('abc', 'bc') // returns true
+//solution('abc', 'd') // returns false
+
+//Top 3 solutions
+//1
+function solution(str, ending){
+    return str.endsWith(ending);
+  }
+//2
+const solution = (str, ending) => str.endsWith(ending);
+//3
+function solution(str, ending){
+    if (typeof(str) != "string" || typeof(ending) != "string")
+      throw "wrong type";
+    if (ending.length>str.length)
+      return false;
+    return str.substr(str.length-ending.length, ending.length) == ending;
+  }
