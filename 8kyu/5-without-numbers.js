7a6cce6bb3e658653707b0f032a023cb39b66170
MondoBazoo <isaiahrivera850@gmail.com>
2022-08-03 22:55:51 -0700

    added new codewars challenge

---
+++ 8kyu/5-without-numbers.js
@@ -0,0 +1,14 @@
+//Write a function that always returns 5
+//Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+//Top 3 solutions
+//1
+function unusualFive() {
+    return 'fucku'.length
+  }
+//2
+function unusualFive() {
+    return 'DaniQ'.length
+  }
+//3
+const unusualFive = () => ['Putin', 'huilo', 'lalala', 'lalala', 'lalala'].length
