26d85daeb9ddd9a92a5b3578c0d9dddd9f2f79d4
MondoBazoo <isaiahrivera850@gmail.com>
2022-08-16 14:37:53 -0700

    added new codewars challenge

---
+++ 8kyu/square-n-sum.js
@@ -0,0 +1,22 @@
+//Complete the square sum function so that it squares each number passed into it and then sums the results together.
+//For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+//Top 3 solutions
+//1
+function squareSum(numbers){
+    return numbers.reduce(function(sum, n){
+      return (n*n) + sum;
+    }, 0)
+  }
+//2
+function squareSum(numbers){
+    return numbers.reduce((sum,num) => sum + (num * num), 0);
+  }
+//3
+function squareSum(numbers){
+    var sum = 0;
+    numbers.forEach(function(n) {
+      sum += n * n
+    });
+    return sum;
+  }
