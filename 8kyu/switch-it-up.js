5382990b822fd725ff6084b37976b40a9a3bbb02
MondoBazoo <isaiahrivera850@gmail.com>
2022-09-23 16:08:55 -0700

    added new codewars challenge

---
+++ 8kyu/switch-it-up.js
@@ -0,0 +1,49 @@
+//When provided with a number between 0-9, return it in words.
+//Input :: 1
+//Output :: "One".
+
+//Top 3 solutions 
+//1
+switchItUp=n=>["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"][n]
+//2
+function switchItUp(number){
+    switch(number) {
+      case 0:
+        return 'Zero';
+      case 1:
+        return 'One';
+      case 2:
+        return 'Two';
+      case 3:
+        return 'Three';
+      case 4:
+        return  'Four';
+      case 5:
+        return 'Five';
+      case 6:
+        return 'Six';
+      case 7:
+        return 'Seven';
+      case 8:
+        return 'Eight';
+      case 9:
+        return 'Nine';
+      default:
+        return 'Unknown number';
+    }
+  }
+//3
+function switchItUp(number) {
+    switch (number) {
+      case 0: return 'Zero';
+      case 1: return 'One';
+      case 2: return 'Two';
+      case 3: return 'Three';
+      case 4: return 'Four';
+      case 5: return 'Five';
+      case 6: return 'Six';
+      case 7: return 'Seven';
+      case 8: return 'Eight';
+      case 9: return 'Nine';
+    }
+  }
