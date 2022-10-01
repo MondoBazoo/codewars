850a92f56ec02dba95a81868a7c6f216c3ca9ec3
MondoBazoo <isaiahrivera850@gmail.com>
2022-10-01 14:43:58 -0700

    added new codewars solution

---
+++ 8kyu/you-cant-code-underpressure.js
@@ -0,0 +1,18 @@
+//Code as fast as you can! You need to double the integer and return it.
+
+//My solution
+function doubleInteger(i) {
+    return i*2;
+  }
+//1
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i * 2;
+  }
+//2
+const doubleInteger = i => 2 * i;
+//3
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i+i;
+  }
