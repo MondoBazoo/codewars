f001feda8357147d12916548e3287f798203c6ba
MondoBazoo <isaiahrivera850@gmail.com>
2022-10-17 17:46:58 -0700

    added codewars solution

--- 8kyu/square-n-sum.js
+++ 8kyu/square-n-sum.js
@@ -1,6 +1,9 @@
 //Complete the square sum function so that it squares each number passed into it and then sums the results together.
 //For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 
+
+//My solution
+
 //Top 3 solutions
 //1
 function squareSum(numbers){
