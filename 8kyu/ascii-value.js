8ce8bedcbc49ec4555296f982e45eb6ab2901e88
MondoBazoo <isaiahrivera850@gmail.com>
2022-11-23 16:38:34 -0800

    added codewars solution

---
+++ 8kyu/ascii-value.js
@@ -0,0 +1,22 @@
+//Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+
+//Example:
+
+//get_char(65)
+//should return:
+
+//'A'
+
+//My solution
+const getChar = String.fromCharCode;
+
+//Top 3 solutions
+//1
+const getChar = String.fromCharCode;
+//2
+function getChar(c) {
+    return String.fromCharCode(c)
+  }
+//3
+var getChar = c => String.fromCharCode(c);
+
