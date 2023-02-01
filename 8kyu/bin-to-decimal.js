15a6033e891debaa6a1721523e471d00048170d2
MondoBazoo <isaiahrivera850@gmail.com>
2023-01-31 18:34:02 -0800

    added codewars solution

---
+++ 8kyu/bin-to-decimal.js
@@ -0,0 +1,16 @@
+//Complete the function which converts a binary number (given as a string) to a decimal number.
+
+//My solution
+function binToDec(bin){
+    return parseInt(bin, 2);
+  }
+
+//Top 3 solutions
+//1
+function binToDec(bin){
+    return parseInt(bin,2);
+  }
+//2
+const binToDec = bin => parseInt(bin,2);
+//3
+binToDec=n=>parseInt(n,2)
