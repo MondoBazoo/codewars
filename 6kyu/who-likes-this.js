476de28eae535e94e2f5a1620f56444b7e2d8ff9
MondoBazoo <isaiahrivera850@gmail.com>
2023-02-24 22:10:21 -0800

    added codewars solution

--- 6kyu/who-likes-this.js
+++ 6kyu/who-likes-this.js
@@ -9,16 +9,16 @@ Implement the function which takes an array containing the names of people that 
 ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this" */
 
 //My solution
-function likes(names) {
-    names = names || [];
-    switch(names.length){
-      case 0: return 'no one likes this'; break;
-      case 1: return names[0] + ' likes this'; break;
-      case 2: return names[0] + ' and ' + names[1] + ' like this'; break;
-      case 3: return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this'; break;
-      default: return names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this';
-    }
+function likes(names) {g
+  names = names || [];
+  switch(names.length){
+    case 0: return 'no one likes this'; break;
+    case 1: return names[0] + ' likes this'; break;
+    case 2: return names[0] + ' and ' + names[1] + ' like this'; break;
+    case 3: return names[0] + ', ' + names[1] + ' and ' + names[2] + ' like this'; break;
+    default: return names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this';
   }
+}
 //Top 3 solutions
 //1
 function likes(names) {
