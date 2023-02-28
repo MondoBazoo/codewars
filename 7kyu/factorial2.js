ed799aa1c76d8ab6e6d55bb720cdfcb8cb6f9c0d
MondoBazoo <isaiahrivera850@gmail.com>
2023-02-27 21:08:11 -0800

    added codewars solution

--- 7kyu/factorial2.js
+++ 7kyu/factorial2.js
@@ -2,6 +2,19 @@
 
 //https://en.wikipedia.org/wiki/Factorial
 
+//My solution
+const factorial = n => n ? factorial(n - 1) * n : 1;
+//2
+function factorial(n){
+    let answer = 1;
+    
+    while (n > 0) {
+      answer *= n;
+      n--;
+    }
+    
+    return answer;
+  }
 //Top 3 solutions
 //1
 const factorial = n => n ? factorial(n - 1) * n : 1;
