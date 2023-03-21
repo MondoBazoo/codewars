e4b543db5c9f06bb79647d7e57915f945a0a4eeb
MondoBazoo <isaiahrivera850@gmail.com>
2023-03-20 21:38:29 -0700

    add: codewars solution

--- 7kyu/sort-number.js
+++ 7kyu/sort-number.js
@@ -5,6 +5,10 @@
 //solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
 //solution(null); // should return []
 
+//My solution
+function solution(nums){
+  return nums !== null ? nums.sort(function(a,b){return a-b}) : [];
+}
 //Top 3 solutions
 //1
 function solution(nums){
