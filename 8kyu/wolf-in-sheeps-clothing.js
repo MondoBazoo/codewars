fd90521ef90b0deda2f123cd5880c71d8f2edf96
MondoBazoo <isaiahrivera850@gmail.com>
2023-03-28 21:20:56 -0700

    add: codewars challenge

--- 8kyu/wolf-in-sheeps-clothing.js
+++ 8kyu/wolf-in-sheeps-clothing.js
@@ -17,7 +17,10 @@ Output: "Oi! Sheep number 1! You are about to be eaten by a wolf!"
 Input: ["sheep", "sheep", "wolf"]
 Output: "Pls go away and stop eating my sheep"
 */
-
+//My solution
+function warnTheSheep(q) {
+    return q[q.length-1] === 'wolf' ? "Pls go away and stop eating my sheep" : `Oi! Sheep number ${q.length - (q.indexOf('wolf')+1)}! You are about to be eaten by a wolf!`
+  }
 //Top 3 solutions
 //1
 function warnTheSheep(queue) {
