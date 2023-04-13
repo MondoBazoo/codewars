808a16ae1f41e38cbb105653ec49638df8ce94a7
MondoBazoo <isaiahrivera850@gmail.com>
2023-04-12 21:32:04 -0700

    add: codewars solution

--- 8kyu/parse-nice-int.js
+++ 8kyu/parse-nice-int.js
@@ -2,6 +2,10 @@
 //Write a program that returns the girl's age (0-9) as an integer.
 //Assume the test input string is always a valid string. For example, the test input may be "1 year old" or "5 years old". The first character in the string is always a number.
 
+//My solution
+function getAge(inputString){
+  return parseInt(inputString);
+}
 //Top 3 solutions
 //1
 function getAge(inputString){
