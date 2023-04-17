2082ab833d1345ae458cd0c868ea55f7eb1130cd
MondoBazoo <isaiahrivera850@gmail.com>
2023-04-16 18:51:49 -0700

    add: codewars solution

--- 6kyu/detect-panagram.js
+++ 6kyu/detect-panagram.js
@@ -25,3 +25,10 @@ function isPangram(string){
 function isPangram(string){
     return (string.match(/([a-z])(?!.*\1)/ig) || []).length === 26;
   }
+
+//4
+function isPangram(string){
+  return 'abcdefghijklmnopqrstuvwxyz'
+    .split('')
+    .every((x) => string.toLowerCase().includes(x));
+}
