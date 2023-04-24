4523b7ef955d89dff0b96d00637a25cd2376de48
MondoBazoo <isaiahrivera850@gmail.com>
2023-04-23 22:13:42 -0700

    add: codewars solution

---
+++ 8kyu/basic-function-fixer.js
@@ -0,0 +1,23 @@
+//Fix the function
+
+//I created this function to add five to any number that was passed in to it and return the new value. It doesn't throw any errors but it returns the wrong number.
+
+//Can you help me fix the function?
+
+//My solution
+function addFive(num) {
+    return num + 5;
+  }
+//Top 3 solutions
+//1
+function addFive(num) {
+    return num + 5;
+  }
+//2
+const addFive = n => n + 5;
+//3
+function addFive(num) {
+    var total = num + 5;
+    return total;
+  }
+  
