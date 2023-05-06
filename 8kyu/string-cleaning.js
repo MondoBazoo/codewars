43193e4846408653d00aef7782e30d3105195bd7
MondoBazoo <isaiahrivera850@gmail.com>
2023-05-05 22:40:17 -0700

    add: codewars solution

--- 8kyu/string-cleaning.js
+++ 8kyu/string-cleaning.js
@@ -7,6 +7,10 @@ Examples (input -> output)
 'This looks5 grea8t!' -> 'This looks great!'
 Your harried co-workers are looking to you for a solution to take this garbled text and remove all of the numbers. Your program will take in a string and clean out all numeric characters, and return a string with spacing and special characters ~#$%^&!@*():;"'.,? all intact. */
 
+//My solution
+function stringClean(s){
+  return s.replace(/[0-9]/g, '');
+}
 //Top 3 solutions
 //1
 function stringClean(s){
