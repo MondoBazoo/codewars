208f1f60951140bf7d2c21e785cd636507970913
MondoBazoo <isaiahrivera850@gmail.com>
2023-05-31 21:00:56 -0700

    add: codewars solution

--- 8kyu/thinkful-logic-drills.js
+++ 8kyu/thinkful-logic-drills.js
@@ -45,3 +45,14 @@ function updateLight(current) {
     }
     
   }
+  //4
+  function updateLight(current) {
+    switch(current){
+        case 'green':
+        return 'yellow';
+        case 'yellow':
+        return 'red';
+        case 'red':
+        return 'green';
+    }
+  }
